4a48f5aa5ec70952ab620fcdf4ae2af84436990a
taojianhang <1811783973@qq.com>
2022-04-07 14:40:03 +0800

    修改vite配置

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"mtk-ui-dom-test",
   plugins: [vue()],
+  build: {
+    outDir: "build",
+  },
 });
