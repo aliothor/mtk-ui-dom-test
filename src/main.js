86c7d8d5c1c2982b30525583635b4c6ec337c024
taojianhang <1811783973@qq.com>
2022-04-07 15:22:57 +0800

    修改配置按需加载antv

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@
  * @Author: pluto
  * @Date: 2022-04-07 14:24:04
  * @LastEditors: pluto
- * @LastEditTime: 2022-04-07 15:02:12
+ * @LastEditTime: 2022-04-07 15:21:03
  * @Description: 
  * 
  */
 import { createApp } from "vue";
 import App from "./App.vue";
 import "ant-design-vue/dist/antd.css";
-import Antd from "ant-design-vue";
+// import Antd from "ant-design-vue";
 
 const app = createApp(App);
 
-app.use(Antd);
+// app.use(Antd);
 app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/mtk-ui-dom-test/",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
   build: {
     outDir: "build",
   },
